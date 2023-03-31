8bb1c30a9d0e362a9e59bdab7d0c1a01a3ccbe8c
Bloom rvls <angelicaravelonjohanison@gmail.com>
2023-03-31 06:04:32 +0300

    test card

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Fantaro'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +31,146 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Text(
+          widget.title,
+        ),
+        centerTitle: true,
+
+        // ignore: prefer_const_constructors
+        leading: IconButton(
+          // ignore: prefer_const_constructors
+          icon: Icon(
+            Icons.density_medium_rounded,
+            color: Colors.white,
+            size: 20,
+          ),
+          onPressed: null,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              buildCard(),
+              buildCard(),
+              buildCard(),
+              buildCard(),
+              buildCard(),
+              buildCard(),
+            ],
+          ),
+        ),
+      ),
+
+      /*body: Center(
+        child: Card(
+          elevation: 20,
+          shadowColor: Colors.black,
+          color: Colors.white,
+          child: SizedBox(
+            width: 300,
+            height: 500,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.purple,
+                    radius: 108,
+                    child: const CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: 100,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Fantaro project',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
+                      onPressed: () => 'Null',
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                      ), 
+                      child: Padding(
+                        padding: const EdgeInsets.all(4),
+                        child: Row(
+                          children: const[
+                            Icon(Icons.touch_app),
+                            Text('Visit')
+                          ],
+                        ), 
+                        ),
+                    ),
+                  ),
+                ],
+              ), 
+            ),
+          ),
+        ),
+        
+      ),*/
+    );
+  }
+
+  Card buildCard() {
+    var heading = '\$1500 per mounth';
+    var subheading = '2 bed, 1 bath';
+    var cardImage = const AssetImage('images/compostage.jpg');
+    var supportingText = 'beautiful card ...';
+
+    return Card(
+      elevation: 4.0,
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(heading),
+            subtitle: Text(subheading),
+            trailing: Icon(Icons.favorite_outline),
+          ),
+          Container(
+            height: 80.0,
+            child: Ink.image(
+              image: cardImage,
+              fit:  BoxFit.cover,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.centerLeft,
+            child: Text(supportingText),
+          ),
+          ButtonBar(
+            children: [
+              TextButton(
+                onPressed: () {/* */}, 
+                child: const Text('Contact')
+              ),
+              TextButton(
+                onPressed: () {/* */}, 
+                child: const Text('Learn more')
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
