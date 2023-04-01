d02044df606ebe6980730b7adc00679cdfa65e12
Bloom rvls <angelicaravelonjohanison@gmail.com>
2023-04-01 15:48:53 +0300

    article list seconde version modification du widget card ajout border radius

--- lib/components/article_card.dart
+++ lib/components/article_card.dart
@@ -12,62 +12,71 @@ class ArticleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      margin: const EdgeInsets.all(20.0),
-      shadowColor: Colors.black,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Expanded(
-            child: Image.asset(article.imageUrl),
-          ),
-          
-          Expanded(
-            child: Column(
-              children: [
-              Row( 
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const [
-                  IconButton(
-                    icon: Icon(
-                      Icons.archive_sharp,
-                      size: 30,
-                      color: Colors.black,
-                    ),
-                    onPressed: null, 
-                              
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Icon(Icons.circle_rounded, color: Colors.yellow,size: 15,),
-                  Icon(Icons.circle_rounded, color: Colors.green, size: 15,),
-                  Icon(Icons.circle_rounded, color: Colors.red,size: 15,),
-                ],
-              ),
-              Container(
-                padding: const EdgeInsets.all(10.0),
-                alignment: Alignment.center,
-                child: Text(
-                  article.title,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10.0),
-                alignment: Alignment.centerLeft,
-                child: Text(Article.slice(article.details)),
-              ),
-              
-            ],
+    return InkWell(
+      onTap: () {
+        const Text("card cliquer une fois");
+      },
+      child: Card(
+        elevation: 4.0,
+        margin: const EdgeInsets.all(20.0),
+        shadowColor: Colors.black,
+
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [  
+            Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      IconButton(
+                        icon: Icon(
+                          Icons.archive_sharp,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                        onPressed: null,
+                      ),
+                    ],
             ),
-            
-          ),
-        ],
+            Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(
+                        Icons.circle_rounded,
+                        color: Colors.yellow,
+                        size: 10,
+                      ),
+                      Icon(
+                        Icons.circle_rounded,
+                        color: Colors.green,
+                        size: 10,
+                      ),
+                      Icon(
+                        Icons.circle_rounded,
+                        color: Colors.red,
+                        size: 10,
+                      ),
+                    ],
+            ),
+
+            ListTile(
+              leading: Image.asset(
+                article.imageUrl,
+                
+                
+              ),
+              title: Text(
+                article.title,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              subtitle: Text(Article.slice(article.details)),
+                
+            ),
+          ],
+        ),
       ),
     );
   }
