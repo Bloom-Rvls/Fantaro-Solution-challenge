9e2697b42fc101dad3ae552efa5c98504e875c25
unknown <gianierama@gmail.com>
2023-04-02 11:20:05 +0300

    Add navig ver1

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fantaro',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
@@ -21,13 +21,14 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(),
-      appBar: AppBar(
-        title: Image.asset('assets/images/icon.png'),
-      ),
-      body: Center(
-        child: Text('Bienvenue sur Fantaro'),
-      ),
-    );
+        drawer: NavDrawer(),
+        appBar: AppBar(
+          title: Image.asset('assets/images/icon.png'),
+        ),
+        body: Container(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: Center(
+              child: Text('Hello, world!' '\n' 'Hello, world!'),
+            )));
   }
 }
--- lib/widgets/nav-drawer.dart
+++ lib/widgets/nav-drawer.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:namer_app/widgets/aboutApp.dart';
+import 'package:namer_app/widgets/aboutUs.dart';
+
+import '../widgets/archive.dart';
 
 class NavDrawer extends StatelessWidget {
   @override
@@ -9,64 +13,73 @@ class NavDrawer extends StatelessWidget {
         children: <Widget>[
           DrawerHeader(
             child: Text(
-              'Accueil',
+              ' \n        Accueil',
               style: TextStyle(
                 color: Color.fromARGB(240, 224, 223, 231),
-                fontSize: 35,
+                fontSize: 30,
                 height: 2,
                 fontWeight: FontWeight.w200,
               ),
             ),
             decoration: BoxDecoration(
-                color: Color.fromARGB(144, 28, 174, 120),
-                image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage('assets/images/icon1.jpg'))),
+              color: Color.fromARGB(240, 8, 52, 88),
+            ),
           ),
           ListTile(
             leading: Icon(
               Icons.abc_outlined,
-              color: Color.fromARGB(255, 19, 91, 149),
+              color: Color.fromARGB(240, 8, 52, 88),
               size: 24.0,
             ),
             title: Text('Langue'),
             onTap: () => {},
           ),
           ListTile(
-            leading: Icon(Icons.bedtime_sharp,
-                color: Color.fromARGB(255, 19, 91, 149), size: 24.0),
-            title: Text('Mode nuit'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
+              leading: Icon(Icons.bedtime_sharp,
+                  color: Color.fromARGB(240, 8, 52, 88), size: 24.0),
+              title: Text('Mode nuit'),
+              onTap: () => {}),
           ListTile(
-            leading: Icon(
-              Icons.archive_sharp,
-              color: Color.fromARGB(255, 19, 91, 149),
-              size: 24.0,
-              semanticLabel: 'Text to announce in accessibility modes',
-            ),
-            title: Text('Archives'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
+              leading: Icon(
+                Icons.archive_sharp,
+                color: Color.fromARGB(240, 8, 52, 88),
+                size: 24.0,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+              title: Text('Archives'),
+              onTap: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Archive()),
+                    )
+                  }),
           ListTile(
-            leading: Icon(
-              Icons.ad_units_rounded,
-              color: Color.fromARGB(255, 19, 91, 149),
-              size: 24.0,
-              semanticLabel: 'Text to announce in accessibility modes',
-            ),
-            title: Text('A propos de l\'appli'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
+              leading: Icon(
+                Icons.adb_rounded,
+                color: Color.fromARGB(240, 8, 52, 88),
+                size: 24.0,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+              title: Text('A propos de l\'appli'),
+              onTap: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AboutApp()),
+                    )
+                  }),
           ListTile(
-            leading: Icon(
-              Icons.bookmark,
-              color: Color.fromARGB(255, 19, 91, 149),
-              size: 24.0,
-            ),
-            title: Text('A props de nous'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
+              leading: Icon(
+                Icons.bookmark,
+                color: Color.fromARGB(240, 8, 52, 88),
+                size: 24.0,
+              ),
+              title: Text('A props de nous'),
+              onTap: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AboutUs()),
+                    )
+                  }),
         ],
       ),
     );
